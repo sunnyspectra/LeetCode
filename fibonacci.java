8ec7efe1097a2f3248a39db107983abf95f8dc78
Sunny L <93362448+sunnyspectra@users.noreply.github.com>
2023-09-13 20:50:46 -0700

    Create fibonacci.java
    fibonacci for java

---
+++ fibonacci.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static long[] generateFibonaccis(int n) {
+        long[] fib = new long[n];
+        fib[0] = 1;
+        fib[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            fib[i] = fib[i - 2] + fib[i - 1];
+        }
+        return fib;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        long[] fib = generateFibonaccis(n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(Long.toUnsignedString(fib[i]) + " ");
+        }
+    }
+}
